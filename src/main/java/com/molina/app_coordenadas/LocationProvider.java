e36e2d6688bfb1ccaf393201f5aa0085e4c565e5
Molina25 <molinadennis246@gmail.com>
2020-11-27 20:00:19 -0600

    Agregando layout

---
+++ src/main/java/com/molina/app_coordenadas/LocationProvider.java
@@ -0,0 +1,7 @@
+package com.molina.app_coordenadas;
+
+public class LocationProvider {
+    public static final int AVAILABLE =10 ;
+    public static final int OUT_OF_SERVICE =10 ;
+    public static final int TEMPORARILY_UNAVAILABLE =10 ;
+}
